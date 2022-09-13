356ed16aa8809077cc54f79bff187f7a4dd3b7f6
Adriana <adrianascosta9@gmail.com>
2022-09-13 16:59:06 -0300

    exerciciocompleto

---
+++ exercicio2/script.js
@@ -0,0 +1,6 @@
+let numero = Number(prompt("Digite o número da tabuada que deseja receber?"))
+tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let i in tabuada) {
+    console.log (tabuada[i]*numero)
+    
+}
